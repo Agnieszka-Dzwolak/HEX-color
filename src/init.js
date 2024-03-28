6001e80ead3e9e3ab50c0d1f4a425ecb503a930e
Agnieszka-Dzwolak <dzwolak.agnieszka@gmail.com>
2024-03-28 15:26:32 +0100

    first commit

--- src/init.js
+++ src/init.js
@@ -1,1 +1,32 @@
-alert('init.js loaded!');
+//data
+const data = {
+  characters: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+};
+
+//DOM
+const dom = {
+  btn : document.querySelector('.js-btn'),
+  display : document.querySelector('.js-display'),
+  body : document.body
+};
+
+//handlers
+const changeColor = () => {
+  let hexColor = '#';
+  for(let i = 0; i < 6; i++) {
+    hexColor += getRandomCharacter();
+  }
+  console.log(hexColor)
+  dom.body.style.backgroundColor = hexColor;
+  dom.display.innerHTML = `Background color: ${hexColor}`;
+  dom.display.style.display = 'block';
+}
+
+//utils
+const getRandomCharacter = () => {
+  let randomCharacterIndex = Math.floor(Math.random() * data.characters.length);
+  return data.characters[randomCharacterIndex]
+}
+
+//event listeners
+dom.btn.addEventListener('click', changeColor)
